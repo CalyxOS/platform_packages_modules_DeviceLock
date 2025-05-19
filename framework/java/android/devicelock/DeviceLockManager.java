32eb881dc211959c965a8e1235833b78452b733c
Warren Ochibobo <ochibobow@google.com>
2025-05-18 22:52:16 -0700

    [DeviceLockManager API] Update notifyKioskSetupFinished() docs to explicitly show the exceptions returned/thrown.
    Bug: 411033083
    Bug: 336360411
    Test: NO TEST
    Flag: EXEMPT Java Doc API changes
    Change-Id: I08022624758ef37fa01fe0244823b35e8a869120

--- framework/java/android/devicelock/DeviceLockManager.java
+++ framework/java/android/devicelock/DeviceLockManager.java
@@ -459,8 +459,21 @@ public final class DeviceLockManager {
      * device provision state moving from kiosk_provisioned to
      * provision_success.
      *
+     * <p>Exceptions that can be returned through the ParcelableException on the callback's
+     * {@code onError} method:
+     * <ul>
+     *     <li>{@link SecurityException} if the caller is missing the
+     *     {@link android.android.Manifest.permission.MANAGE_DEVICE_LOCK_STATE} permission.
+     *     <li>{@link IllegalStateException} if the device has already been cleared or if
+     *     policies could not be enforced for the lock or unlock state.
+     *     <li>{@link java.util.concurrent.TimeoutException} if the response from the
+     *     underlying binder call is not received within the specified duration
+     *     (10 seconds).
+     * </ul>
+     *
      * @param executor the {@link Executor} on which to invoke the callback.
      * @param callback this returns either success or an exception.
+     * @throws RuntimeException if there are binder communications errors
      */
     @RequiresPermission(permission.MANAGE_DEVICE_LOCK_STATE)
     @FlaggedApi(FLAG_NOTIFY_KIOSK_SETUP_FINISHED)
