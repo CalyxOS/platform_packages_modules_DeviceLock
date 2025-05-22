7bdd8e90db6b23478c9eefcecf247822cda097af
Warren Ochibobo <ochibobow@google.com>
2025-05-22 01:19:50 -0700

    [DeviceLockManager API] Update unlockDevice() docs to explicitly show the exceptions returned/thrown
    Bug: 417690384
    Test: NO TEST
    Flag: EXEMPT Java API Doc update
    Change-Id: Ic4932e28dc79c9e8394a39cf86a679de55036664

--- framework/java/android/devicelock/DeviceLockManager.java
+++ framework/java/android/devicelock/DeviceLockManager.java
@@ -189,10 +189,23 @@ public final class DeviceLockManager {
     }
 
     /**
-     * Unlock the device.
+     * Unlocks the device.
+     *
+     * <p>Exceptions that can be returned through the ParcelableException on the callback's
+     * {@code onError} method:
+     * <ul>
+     *     <li>{@link SecurityException} if the caller is missing the
+     *     {@link android.android.Manifest.permission.MANAGE_DEVICE_LOCK_STATE} permission.
+     *     <li>{@link IllegalStateException} if the device has already been cleared or if
+     *     policies could not be enforced for the unlock state.
+     *     <li>{@link java.util.concurrent.TimeoutException} If the response from the
+     *     underlying binder call is not received within the specified duration
+     *     (10 seconds).
+     * </ul>
      *
      * @param executor the {@link Executor} on which to invoke the callback.
      * @param callback this returns either success or an exception.
+     * @throws RuntimeException if there are binder communications errors
      */
     @RequiresPermission(permission.MANAGE_DEVICE_LOCK_STATE)
     public void unlockDevice(@NonNull @CallbackExecutor Executor executor,
