0a06ec8a92e228991fef3cf6a89df9f13790c85d
Daniel Musila <dmusila@google.com>
2025-09-12 13:39:21 +0000

    Fix[DLC system_server]: cleanup resources when no longer in use
    Corrects an issue where connectors were not properly closed when no longer used, for example, after the associated user has been removed. Additionally, the handler thread on the connector will be safely quit when the connector is no longer required.
    
    Bug: 427208905
    Flag: EXEMPT BUGFIX
    Test: atest DeviceLockUnitTests
    Test: atest DeviceLockControllerGoogleInstrumentationTests
    Test: manually run `adb shell ps -eT $(adb shell pidof system_server) | grep DeviceLockContr` after adding, switching, and removing secondary users. The thread count should not exceed the number of users created.
    
    Change-Id: I5d950b69e4fa95d71393f520902c401cff018495

--- service/java/com/android/server/devicelock/DeviceLockServiceImpl.java
+++ service/java/com/android/server/devicelock/DeviceLockServiceImpl.java
@@ -384,6 +384,12 @@ final class DeviceLockServiceImpl extends IDeviceLockService.Stub {
             // that a protected package cannot be disabled (but we're actually trying to enable it).
         }
         synchronized (this) {
+            // Cleanup any existing instances before refreshing the connector
+            DeviceLockControllerConnector dlcConnector = mDeviceLockControllerConnectors
+                    .get(userHandle.getIdentifier());
+            if (dlcConnector instanceof DeviceLockControllerConnectorImpl) {
+                ((DeviceLockControllerConnectorImpl) dlcConnector).close();
+            }
             // Refresh connector
             mDeviceLockControllerConnectors.put(userHandle.getIdentifier(), null);
             getDeviceLockControllerConnector(userHandle);
@@ -393,6 +399,17 @@ final class DeviceLockServiceImpl extends IDeviceLockService.Stub {
     void onUserAdded(@NonNull UserHandle userHandle) {
         // New users do not have any provisioning to clean up and can be disabled immediately
         disableDlcIfNeeded(userHandle);
+    }
+
+    void onUserRemoved(@NonNull UserHandle userHandle) {
+        synchronized (this) {
+            DeviceLockControllerConnector connector = mDeviceLockControllerConnectors
+                    .get(userHandle.getIdentifier());
+            if (connector instanceof DeviceLockControllerConnectorImpl) {
+                ((DeviceLockControllerConnectorImpl) connector).close();
+            }
+            mDeviceLockControllerConnectors.remove(userHandle.getIdentifier());
+        }
     }
 
     void onUserSwitching(@NonNull UserHandle userHandle) {
@@ -1256,5 +1273,10 @@ final class DeviceLockServiceImpl extends IDeviceLockService.Stub {
         final Bundle result = new Bundle();
         result.putBoolean(KEY_REMOTE_CALLBACK_RESULT, true);
         remoteCallback.sendResult(result);
+    }
+
+    @VisibleForTesting
+    int getDeviceLockControllerConnectorsSize() {
+        return mDeviceLockControllerConnectors.size();
     }
 }
